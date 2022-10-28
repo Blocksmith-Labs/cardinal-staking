6c61609f8d42a159e03705c860cdcc4ebb90f50a
wozgun <wozgun@protonmail.com>
2022-10-28 18:34:21 +0300

    small

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   ignorePatterns: ["dist/", "*.js"],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: "tsconfig.json",
-  },
+  // parserOptions: {
+  //   tsconfigRootDir: __dirname,
+  //   project: "tsconfig.json",
+  // },
   extends: ["@saberhq"],
   env: {
     node: true,
