ddfec5d37a9c20ca12d3a41c387e8012d2c26be1
wozgun <wozgun@protonmail.com>
2022-10-29 16:08:41 +0300

    fix: remove postinstall

--- dist/esm/programs/rewardDistributor/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Wallet } from "@saberhq/solana-contrib";
-import type { AccountMeta, Connection, PublicKey, Transaction } from "@solana/web3.js";
-import { RewardDistributorKind } from "./constants";
-export declare const withRemainingAccountsForKind: (transaction: Transaction, connection: Connection, wallet: Wallet, rewardDistributorId: PublicKey, kind: RewardDistributorKind, rewardMint: PublicKey, isClaimRewards?: boolean) => Promise<AccountMeta[]>;
-//# sourceMappingURL=utils.d.ts.map
